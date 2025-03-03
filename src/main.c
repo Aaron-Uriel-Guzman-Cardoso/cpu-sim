9b886a656a2d90cab4271ab3d77a0315cafe16a1
LuisGaleana9 <LuisGaleana99@gmail.com>
2025-03-02 18:05:26 -0600

    Implementacion de la window_messages

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include <ctype.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ncurses.h>
 
 #include "../include/cpu.h"
 
@@ -36,37 +37,63 @@ prompt(void)
     return cmd;
 }
 
+//Funcion para imprimir mensajes en la ventana de messages
+void messages_win(WINDOW *messages, const char *mensaje) {
+    box(messages, 0, 0); 
+
+    mvwprintw(messages, 0, 35, "|Messages|");
+
+    mvwprintw(messages, 1, 2, "%s", mensaje);
+
+    wrefresh(messages);
+}
+
+
 /*
- * Evalua el comando pasado como argumento.
- * Todas las acciones que se podrán realizar desde el prompt serán
- * implementadas en esta función.
+ *Prototipo: int32_t eval(struct instruction *cmd, WINDOW *messages, PCB *pcb) {
+ *Propósito: Esta función evalúa e interpreta un comando ingresado por el usuario
+ *y ejecuta la acción correspondiente, como salir del programa o cargar un archivo en el PCB.
+ *Entradas: cmd(Puntero con la instruccion leida), messages(Puntero a la ventana messages) y pcb 
+ *(Puntero a la estructura PCB)
+ *Salidas: Retorna 0 en caso de exito, 1 en caso de error y -1 en caso de un comando no reconocido.
+ *Descripción: Evalúa el comando ingresado y realiza la acción correspondiente. Si el comando es "EXIT" o "SALIR",
+ * cierra el programa. Si el comando es "LOAD", intenta cargar el archivo especificado. Si el comando es nulo o no
+ * reconocido, muestra un mensaje de error.
  */
-int32_t
-eval(struct cmd *cmd) {
-    if (!cmd) { return -1; }
-    if (strncmp(cmd->name, "EXIT", 4) == 0 ||
-        strncmp(cmd->name, "SALIR", 5) == 0) {
-        exit(0);
+
+int32_t 
+eval(struct cmd *cmd, WINDOW *messages) {
+    if (!cmd) { 
+        messages_win(messages, "Error: Comando nulo.");
+        return -1;
     }
+
+    if (strncmp(cmd->name, "EXIT", 4) == 0 || strncmp(cmd->name, "SALIR", 5) == 0) {
+        messages_win(messages, "Saliendo del programa...\n");
+        wrefresh(messages);
+        exit(0);
+    } 
     else if (strncmp(cmd->name, "LOAD", 4) == 0) {
         if (cmd->arg1[0] == '\0') {
-            printf("Error: Falta el nombre del archivo\n");
+            messages_win(messages, "Error: Falta el nombre del archivo\n");
             return 1;
-        }
-        else {
-            printf("Cargando archivo %s\n", cmd->arg1);
+        } else {
+            char mensaje[300];
+            snprintf(mensaje, sizeof(mensaje), "Cargando archivo: %s\n", cmd->arg1);
+            messages_win(messages, mensaje);
+
             FILE *file = fopen(cmd->arg1, "r");
             if (file) {
-                char line[256];
-
+                messages_win(messages, "Archivo cargado con éxito.\n");
                 fclose(file);
-            }
-            else {
-                printf("Error: No se pudo abrir el archivo %s\n",
-                        cmd->arg1);
+            } else {
+                snprintf(mensaje, sizeof(mensaje), "Error: No se pudo abrir el archivo %s\n", cmd->arg1);
+                messages_win(messages, mensaje);
             }
         }
     }
+
+    return 0;
 }
 
 int
